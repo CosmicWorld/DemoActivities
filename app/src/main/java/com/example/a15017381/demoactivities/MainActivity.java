c71a845667558d603d0f82172a9e16eea13c3d02
CosmicWorld <15017381@myrp.edu.sg>
2017-04-21 20:46:54 +0800

    Done!

--- app/src/main/java/com/example/a15017381/demoactivities/MainActivity.java
+++ app/src/main/java/com/example/a15017381/demoactivities/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//qs
+
         Button btnDone = (Button) findViewById(R.id.buttonDone);
         btnDone.setOnClickListener(new View.OnClickListener(){
 
